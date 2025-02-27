bc8eb64b6ba50e4dd5b9b2f2f7bd26c64f06672c
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-02-26 17:49:01 -0800

    [UC] Scrolling score UI implemented

---
+++ Assets/_Scripts/UI/CoinCounterUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using DG.Tweening;
+using DG.Tweening.Core.Easing;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextsContainer;
+
+    [Header("Tweening Parameters")]
+    [SerializeField] private float moveAmount;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+
+    private float containerInitPosition;
+
+    private void Awake()
+    {
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextsContainer.localPosition.y;
+    }
+
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}");
+        coinTextsContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(SetCoinContainer(score));
+    }
+
+    private IEnumerator SetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextsContainer.localPosition;
+        coinTextsContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+}
